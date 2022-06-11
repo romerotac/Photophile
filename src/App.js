ba485d9bee3e8392a74f6bf63e618fa8bbdfaa15
romerotac <christian.romero02.cr@gmail.com>
2022-06-10 22:12:13 -0400

    small change on app.js. Now having a small calculator that add two number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  //change state or value inside the reactjs component
+  
+  //first parameter is valuename second function name
+  const [result,setValue] = useState("");
+
+  const [number1,setNumber1] = useState("");
+  const [number2,setNumber2] = useState("");
+
+  function Calculate(){
+
+    setValue(number1 + number2);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <input type = "Number" onChange={(event)=>{
+        setNumber1(Number(event.target.value));
+      }}/>
+      <input type = "Number" onChange={(event)=>{
+        setNumber2(Number(event.target.value));
+      }}/>
+      <button onClick = {Calculate}> Calculate</button>
+      <p>{result}</p>
     </div>
   );
 }
