5304ad85262c4a7c2df80d440eedcbf4d7f3d557
CRTaipe <christian.romero02.cr@gmail.com>
2023-05-12 17:05:02 +0200

    change on login, check document profile correctly

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,23 +10,14 @@ import { addDoc,collection, Firestore, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 import { FirebaseError } from 'firebase/app';
 import { async } from '@firebase/util';
-
-
 import { useDispatch } from 'react-redux';
 import {editProfile} from '../reduxFiles/actions'
-//import {Link} from "react-router-dom";
-//import fontawesome from '@fortawesome/fontawesome-common-types'
-//import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-//import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid'
-
-//fontawesome.library.add(faCheckSquare, faCoffee);
 
 const styleForm = {
     width: '23rem',
     margin:"auto"
     
 };
-
 
 function Login({setIsAuth}){
 
@@ -45,37 +36,36 @@ function Login({setIsAuth}){
     const [profile,setProfile] = useState([]);
 
     // Creating collection for account user
-    const postsCollectionRef = collection(db, 'accounts');
-    //const profileCollectionRef = collection(db, 'profile');
+    const accountsCollectionRef = collection(db, 'accounts');
     
     //getting the data on collection to the account
-    /*
     useEffect(() => {
         const getAccount = async () => {
              
-            const data = await getDocs(postsCollectionRef)
-            setAccount(data.docs.map((doc) => ({ ...doc.data(), })));
+            const data = await getDocs(accountsCollectionRef)
+            .then(response => {
+                const prf = response.docs.map(doc => ({
+                    data:doc.data(),
+                    id:doc.id
+                }))
+                setAccount(prf)
+            })
             
-            const  data2 = await getDocs(profileCollectionRef)
-            setProfile(data2.docs.map((doc) => ({...doc.data(), })));
         };
         getAccount();
     },[]);
     
-    */
+    
 
     //function that check if the user it's already in the database
     function checkUserinDatabase(data){
-    for (let i = 0; i < account.length; i++){
+        var isEqual = account.some((ref) => {
+            return ref.data.id == data
+        })
+        console.log(isEqual)
+        return isEqual
+    }
 
-        if (data === account[i].id){
-            console.log(account[i].id)
-            return;
-        } else {
-            addUser();
-        }
-    }
-    }
     
 
     //sign in with google
@@ -84,7 +74,6 @@ function Login({setIsAuth}){
         signInWithPopup(auth,provider).then((result) => {
             localStorage.setItem("isAuth",true);
             setIsAuth(true);
-            addUser()
             checkUserinDatabase(auth.currentUser.uid);
             localStorage.setItem('userID', auth.currentUser.uid)
             navigate('/userpage');
@@ -105,10 +94,8 @@ function Login({setIsAuth}){
     
         
     const addUser = async () =>{
-        await addDoc(postsCollectionRef, {id:auth.currentUser.uid,name: auth.currentUser.displayName,email:auth.currentUser.email,favoriteCamera:'',state:'', profileImgPath:'',social:{website:'',twitter:'',instagram:'',facebook:'',pinterest:''}});
-       // await addDoc(profileCollectionRef, {id:auth.currentUser.uid,name:auth.currentUser.displayName,state:'',email:auth.currentUser.email,favoriteCamera:'', profileImgPath:'',social:{website:'',twitter:'',instagram:'',facebook:'',pinterest:''}});
+        await addDoc(accountsCollectionRef, {id:auth.currentUser.uid,name: auth.currentUser.displayName,email:auth.currentUser.email,favoriteCamera:'',state:'', profileImgPath:'',social:{website:'',twitter:'',instagram:'',facebook:'',pinterest:''}});
     }
-
     const updateField = event =>{
         setValues({
           ...values,
